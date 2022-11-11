8d553e93656b255ccae8a17c80cba2a8852ccb07
evverenn <evverenn@student.21-school.ru>
2022-11-11 16:34:46 +0300

    develop. Strstr last haystack sym bug fix.

--- src/Functions/s21_strstr.c
+++ src/Functions/s21_strstr.c
@@ -4,15 +4,17 @@ char *s21_strstr (const char *haystack, const char *needle) {
     char* res = NULL;
     char* hay = (char*)haystack;
     for (; *hay != '\0'; hay++) {
-        if (*hay == *needle) {
-            res = hay;
-            char* nd = (char*)needle;
-            for (; *nd != '\0'; nd++, hay++) {
-                if (*hay != '\0') {
-                    res = NULL;
-                    break;
+        if (!res) {
+            if (*hay == *needle) {
+                res = hay;
+                char* nd = (char*)needle;
+                for (; *nd != '\0'; nd++, hay++) {
+                    if (*hay == '\0') {
+                        res = NULL;
+                        break;
+                    }
+                    if (*hay != *nd) res = NULL;
                 }
-                if (*hay != *nd) res = NULL;
             }
         }
     }
