414daff36d36ac9a644a38e17ef7bfccd8831b50
evverenn <evverenn@student.21-school.ru>
2022-11-11 14:32:40 +0300

    develop. Memcpy done.

--- src/Functions/s21_memcpy.c
+++ src/Functions/s21_memcpy.c
@@ -1,1 +1,7 @@
 #include "../s21_string.h"
+void *s21_memcpy (void *dest, const void *src, s21_size_t n) {
+    char* ptr = dest;
+    const char* s = src;
+    while (n-- > 0) *ptr++ = *s++;
+    return dest;
+}
