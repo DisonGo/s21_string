0ed2f0d4364a074315424c5a12c747c2161caecc
evverenn <evverenn@student.21-school.ru>
2022-11-11 14:10:00 +0300

    develop. Strchr done.

--- src/Functions/s21_strchr.c
+++ src/Functions/s21_strchr.c
@@ -1,1 +1,11 @@
 #include "../s21_string.h"
+char *s21_strchr (const char *str, int c) {
+    char* res = NULL;
+    for (; *str != '\0'; str++){
+        if (*str == c) {
+            res = (char*)str;
+            break;
+        }
+    }
+    return res;
+}
