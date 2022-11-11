8e16748984594726cfd21929d45989646755e2ec
evverenn <evverenn@student.21-school.ru>
2022-11-11 15:26:24 +0300

    develop. Strstr done.

--- src/Functions/s21_strstr.c
+++ src/Functions/s21_strstr.c
@@ -1,1 +1,20 @@
 #include "../s21_string.h"
+#include <stdio.h>
+char *s21_strstr (const char *haystack, const char *needle) {
+    char* res = NULL;
+    char* hay = (char*)haystack;
+    for (; *hay != '\0'; hay++) {
+        if (*hay == *needle) {
+            res = hay;
+            char* nd = (char*)needle;
+            for (; *nd != '\0'; nd++, hay++) {
+                if (*hay != '\0') {
+                    res = NULL;
+                    break;
+                }
+                if (*hay != *nd) res = NULL;
+            }
+        }
+    }
+    return res;
+}
