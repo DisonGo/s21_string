a574e9cbb38f2b593c906bfd640a5a0257ecb9a7
evverenn <evverenn@student.21-school.ru>
2022-11-11 14:16:24 +0300

    develop. Strcpy done.

--- src/Functions/s21_strcpy.c
+++ src/Functions/s21_strcpy.c
@@ -1,1 +1,7 @@
 #include "../s21_string.h"
+char *s21_strcpy (char *dest, const char *src) {
+    char* ptr = dest;
+    for (; *src != '\0'; src++, dest++) *dest = *src;
+    *dest = '\0';
+    return ptr;
+}
