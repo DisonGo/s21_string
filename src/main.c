96abfdc42dc066c8b331f159285a7b26c4792d90
evverenn <evverenn@student.21-school.ru>
2022-11-12 21:15:31 +0300

    develop. Sprintf in progress.

--- src/main.c
+++ src/main.c
@@ -1,17 +1,22 @@
 #include "s21_string.h"
+#include "s21_sprintf.h"
 #include "string.h"
 #include <stdio.h>
 #include <stdlib.h>
 int main() {
-    char text[40] = ",, ,  as , d d , , , ,a   s d sssdddd";
-    // printf("%s\n", s21_strstr(text, " "));
-    char delims[5] = " d";
-    char* token = s21_strtok(text, delims);
-    // char* token = strtok(text, delims);
-    while (token != NULL) {
-        printf("%s\n", token);
-        token = s21_strtok(NULL, delims);
-        // token = strtok(NULL, delims);
-    }
+    // char text[100] = "121ADEssdWDA11212";
+    // printf("%s",(char*) s21_trim(text, "1"));
+    // float a = -123222.231;
+    Field fields[100] = {0};
+    read_fields(fields, "sda%-30f");
+    for (int i = 0; i < 10; i++) {
+        printf("spec:%i\n",fields[i].specifier);
+        printf("flag:%i\n",fields[i].flag);
+        printf("width:%i\n",fields[i].width);
+        printf("precise:%i\n",fields[i].precise);
+        printf("length:%i\n",fields[i].length);
+        printf("value:%s\n\n",fields[i].value);
+    } 
+    // printf("sda%-+30f", a);
     return 0;
 }
---
+++ src/s21_sprintf.h
@@ -0,0 +1,44 @@
+#ifndef VALLUE_BUF_SIZE
+#define VALLUE_BUF_SIZE 1024
+#endif  // VALLUE_BUF_SIZE
+
+#ifndef S21_SPRINTF_H_
+#define S21_SPRINTF_H_
+#include "s21_string.h"
+#include <stdlib.h>
+typedef enum _flag_syms {
+    std_f   = 1 << 0,
+    c_f     = 1 << 1,
+    d_f     = 1 << 2,
+    i_f     = 1 << 3,
+    f_f     = 1 << 4,
+    s_f     = 1 << 5,
+    u_f     = 1 << 6,
+    mns_f   = 1 << 7,
+    pls_f   = 1 << 8,
+    blnk_f  = 1 << 9,
+    dot_f   = 1 << 10,
+    h_f     = 1 << 11,
+    l_f     = 1 << 12,
+    prcnt_f = 1 << 13,
+}Flag_syms;
+typedef enum _field_read_states {
+    std_state        = 1 << 0, 
+    flag_state       = 1 << 1, 
+    width_state      = 1 << 2, 
+    precise_state    = 1 << 3, 
+    length_state     = 1 << 4, 
+    specifier_state  = 1 << 5, 
+}Read_states;
+typedef struct _field {
+    int specifier;
+    int flag;
+    int width;
+    int precise;
+    int length;
+    char value[VALLUE_BUF_SIZE];
+}Field;
+Field* read_fields(Field* fields, const char *pattern) ;
+Flag_syms flag_map(int c);
+void throw_pattern_error();
+#endif  // S21_SPRINTF_H_
