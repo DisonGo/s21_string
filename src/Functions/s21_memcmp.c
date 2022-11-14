98321b0b29db04f6a7cde761248a17fcbbd81cf0
burnsdov <burnsdov@student.21-school.ru>
2022-11-14 17:30:53 +0300

    develop. Memcmp done.

--- src/Functions/s21_memcmp.c
+++ src/Functions/s21_memcmp.c
@@ -1,1 +1,11 @@
 #include "../s21_string.h"
+int s21_memcmp(const void *str1, const void *str2, s21_size_t n) {
+    int result = 0;
+    for (s21_size_t i = 0; i < n; i++) {
+        if (!result && *(char*)str1 != *(char*)str2)
+            result = ((*(char*)str1) - (*(char*)str2));
+        str1++;
+        str2++;
+    }
+    return result;
+}
