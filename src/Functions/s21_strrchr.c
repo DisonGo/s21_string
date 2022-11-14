0942e45475844953415a8637dc19998428664c27
burnsdov <burnsdov@student.21-school.ru>
2022-11-14 17:31:24 +0300

    develop. Strrchr done.

--- src/Functions/s21_strrchr.c
+++ src/Functions/s21_strrchr.c
@@ -1,1 +1,8 @@
 #include "../s21_string.h"
+char *s21_strrchr(const char *str, int c) {
+    char* last_pos = S21_NULL;
+    for (s21_size_t i = 0; i < s21_strlen(str); i++) {
+        if (str[i] == c) last_pos = (char*)(str + i);
+    }
+    return last_pos;
+}
