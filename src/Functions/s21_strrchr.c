15afedf7175c8e67fb015f639065846f1783d54c
evverenn <evverenn@student.21-school.ru>
2022-11-14 19:41:12 +0300

    develop_burnsdov. Style fixes.

--- src/Functions/s21_strrchr.c
+++ src/Functions/s21_strrchr.c
@@ -1,8 +1,10 @@
 #include "../s21_string.h"
 char *s21_strrchr(const char *str, int c) {
     char* last_pos = S21_NULL;
-    for (s21_size_t i = 0; i < s21_strlen(str); i++) {
-        if (str[i] == c) last_pos = (char*)(str + i);
+    char* ptr = (char*)str;
+    while (*ptr != '\0') {
+        if (*ptr == c) last_pos = ptr;
+        ptr++;
     }
     return last_pos;
 }
--- src/Functions/s21_strspn.c
+++ src/Functions/s21_strspn.c
@@ -1,15 +1,15 @@
 #include "../s21_string.h"
 s21_size_t s21_strspn(const char* str1, const char* str2) {
-    int flag = 0;
+    int do_loop = 1;
     const char* str1_def = str1;
     s21_size_t char_length = s21_strlen(str2);
     char* _old_pos = S21_NULL;
-    while (!flag) {
+    while (do_loop) {
         _old_pos = (char*)str1;
         for (s21_size_t i = 0; i < char_length && str1 == _old_pos; i++) {
             if (*str1 == str2[i] && str1 == _old_pos) str1++;
         }
-        if (*str1 == '\0' || _old_pos++ == str1) flag = 1;
+        if (*str1 == '\0' || _old_pos++ == str1) do_loop = 0;
     }
     return str1 - str1_def;
 }
