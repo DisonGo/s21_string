3960f8a7a201d30c2f365b34d4317803e43c50f9
burnsdov <burnsdov@student.21-school.ru>
2022-11-14 18:47:47 +0300

    Done.

--- src/Functions/s21_strcmp.c
+++ src/Functions/s21_strcmp.c
@@ -1,8 +1,8 @@
 #include "../s21_string.h"
 int s21_strcmp(const char *str1, const char *str2) {
     int result = 0;
-    s21_size_t str_length = strlen(str1) > strlen(str2) ? strlen(str1) : strlen(str2);
-    for (int i = 0; i < str_length; i++)
+    s21_size_t str_length = s21_strlen(str1) > s21_strlen(str2) ? s21_strlen(str1) : s21_strlen(str2);
+    for (s21_size_t i = 0; i < str_length; i++)
         if (!result && (str1[i] != str2[i])) result = str1[i] - str2[i];
     return result;
 }
